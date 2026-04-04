193660482b6306eaf395ef0284ace1e6c7355f3d
thuyhangg0412-spec <thuyhangg0412@gmail.com>
2026-04-04 15:38:47 +0700

    Implement SignUpScreen UI with user input fields

---
+++ app/src/main/java/com/example/group1spacedrepetitionsystem/ui/feature/auth/SignUpScreen.kt
@@ -0,0 +1,250 @@
+package com.example.estudapp.ui.feature.auth
+
+import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import com.example.estudapp.R
+import com.example.estudapp.ui.theme.ErrorRed
+import com.example.estudapp.ui.theme.PrimaryBlue
+
+@Composable
+fun SignUpScreen(
+    navController: NavHostController,
+    authViewModel: AuthViewModel
+) {
+    var name by remember {
+        mutableStateOf("")
+    }
+
+    var email by remember {
+        mutableStateOf("")
+    }
+
+    var password by remember {
+        mutableStateOf("")
+    }
+
+    var passwordVisible by remember { mutableStateOf(false) }
+
+    val authState = authViewModel.authState.observeAsState()
+    val context = LocalContext.current
+
+    LaunchedEffect(authState.value) {
+        when (authState.value) {
+            is AuthState.Autheticated -> {
+                navController.navigate("home")
+            }
+
+            is AuthState.Error -> Toast.makeText(
+                context,
+                (authState.value as AuthState.Error).message,
+                Toast.LENGTH_SHORT
+            ).show()
+
+            else -> Unit
+        }
+    }
+
+    Scaffold (
+        modifier = Modifier,
+
+        ) { paddingValues ->
+
+        Column (
+            modifier = Modifier
+                .fillMaxSize()
+                .consumeWindowInsets(paddingValues)
+                .padding(top = 16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ){
+            Box(
+                modifier = Modifier
+                    .clip(RoundedCornerShape(50f))
+                    .size(160.dp)
+                    .background(PrimaryBlue),
+                contentAlignment = Alignment.Center
+            ) {
+                Image(painter = painterResource(id = R.drawable.logo_white), contentDescription = "Logo", Modifier.size(90.dp))
+            }
+
+            Spacer(Modifier.height(30.dp))
+
+            Text(text = stringResource(id = R.string.app_name), fontSize = 50.sp, color = PrimaryBlue, fontWeight = FontWeight.Black)
+            Spacer(Modifier.height(30.dp))
+
+            Text(
+                text = stringResource(id = R.string.name_label), color = PrimaryBlue,
+                fontSize = 18.sp,
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .padding(start = 28.dp)
+            )
+            Spacer(Modifier.height(7.dp))
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth(0.85f)
+                    .height(55.dp),
+                value = name,
+                onValueChange = { name = it },
+                placeholder = { Text(stringResource(id = R.string.name_placeholder)) },
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = PrimaryBlue,
+                    unfocusedIndicatorColor = PrimaryBlue,
+                    cursorColor = PrimaryBlue,
+                    errorIndicatorColor = ErrorRed
+                ),
+                shape = RoundedCornerShape(30f),
+                singleLine = true
+            )
+
+            Spacer(Modifier.height(10.dp))
+
+            Text(
+                text = stringResource(id = R.string.email_label), color = PrimaryBlue,
+                fontSize = 18.sp,
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .padding(start = 28.dp)
+            )
+            Spacer(Modifier.height(7.dp))
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth(0.85f)
+                    .height(55.dp),
+                value = email,
+                onValueChange = { email = it },
+                placeholder = { Text(stringResource(id = R.string.email_placeholder)) },
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = PrimaryBlue,
+                    unfocusedIndicatorColor = PrimaryBlue,
+                    cursorColor = PrimaryBlue,
+                    errorIndicatorColor = ErrorRed
+                ),
+                shape = RoundedCornerShape(30f),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
+                singleLine = true
+            )
+
+            Spacer(Modifier.height(10.dp))
+
+            Text(
+                text = stringResource(id = R.string.password_label), color = PrimaryBlue,
+                fontSize = 18.sp,
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .align(Alignment.Start)
+                    .padding(start = 28.dp)
+            )
+            Spacer(Modifier.height(7.dp))
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth(0.85f)
+                    .height(55.dp),
+                value = password,
+                onValueChange = { password = it },
+                placeholder = { Text(stringResource(id = R.string.password_placeholder)) },
+                colors = TextFieldDefaults.colors(
+                    focusedIndicatorColor = PrimaryBlue,
+                    unfocusedIndicatorColor = PrimaryBlue,
+                    cursorColor = PrimaryBlue,
+                    errorIndicatorColor = ErrorRed
+                ),
+                shape = RoundedCornerShape(30f),
+                singleLine = true,
+                visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                trailingIcon = {
+                    val image = if (passwordVisible)
+                        Icons.Filled.Visibility
+                    else Icons.Filled.VisibilityOff
+
+                    val description = if (passwordVisible) stringResource(id = R.string.hide_password) else stringResource(id = R.string.show_password)
+
+                    IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                        Icon(imageVector = image, description, tint = PrimaryBlue)
+                    }
+                }
+            )
+
+            Spacer(Modifier.height(30.dp))
+
+            Button(
+                onClick = { authViewModel.signup(name, email, password)},
+                modifier = Modifier
+                    .fillMaxWidth(0.85f)
+                    .height(55.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = PrimaryBlue
+                ),
+                shape = RoundedCornerShape(30f),
+                enabled = (email.length >= 1 && password.length >= 8 && name.length >= 3)
+            ) {
+                Text(text = stringResource(id = R.string.btn_register), fontSize = 18.sp)
+            }
+
+            Spacer(Modifier.height(10.dp))
+
+            TextButton(
+                onClick = { navController.navigate("login") },
+
+                ) {
+                Text(stringResource(id = R.string.already_have_account), color = PrimaryBlue, fontSize = 15.sp)
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SignUpScreenPreview() {
+    SignUpScreen(navController = NavHostController(LocalContext.current), authViewModel = AuthViewModel())
+}
