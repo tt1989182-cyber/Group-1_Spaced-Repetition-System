77237790218e108373b2277727d5f3cc1a1930ef
Nguyễn Thị Tiên <songngu1442005@gmail.com>
2026-04-04 17:29:33 +0700

    chinh sua mau sac, font chu

--- app/src/main/java/com/example/group1spacedrepetitionsystem/ui/theme/Color.kt
+++ app/src/main/java/com/example/group1spacedrepetitionsystem/ui/theme/Color.kt
@@ -1,4 +1,4 @@
-package com.example.group1spacedrepetitionsystem.ui.theme
+package com.example.estudapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
@@ -9,3 +9,10 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val PrimaryBlue = Color(0xFF5665CE)
+val ErrorRed = Color(0xFFFF1717)
+val LightGray= Color(0xFFDBDEEF)
+val Black = Color(0xFF161616)
+val White = Color(0xFFF5F4F2)
+val DarkBlue = Color(0xFF170334)
--- app/src/main/java/com/example/group1spacedrepetitionsystem/ui/theme/Theme.kt
+++ app/src/main/java/com/example/group1spacedrepetitionsystem/ui/theme/Theme.kt
@@ -1,4 +1,4 @@
-package com.example.group1spacedrepetitionsystem.ui.theme
+package com.example.estudapp.ui.theme
 
 import android.app.Activity
 import android.os.Build
@@ -10,6 +10,12 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+import com.example.estudapp.ui.theme.Typography
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -18,9 +24,10 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = Color(0xFF5665CE),
+    secondary = Color(0xFF391570),
+    tertiary = Pink40,
+    surface = Color(0xFFB9BBC9),
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
@@ -33,9 +40,10 @@ private val LightColorScheme = lightColorScheme(
     */
 )
 
+
 @Composable
-fun Group1SpacedRepetitionSystemTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
+fun EstudaTheme(
+    darkTheme: Boolean = false,
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
